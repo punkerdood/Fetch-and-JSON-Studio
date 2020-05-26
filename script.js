bb8cbead23504cfbed0748a541383ad7ea5a6537
punkerdood <42493482+punkerdood@users.noreply.github.com>
2020-05-26 10:27:18 -0400

    I did

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    let json = []
+    let index = 0
+    let item = ''
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+        let firstName = json[1].firstName;
+        function(){
+        document.getElementsByClassName("astronaut").innerHTML = `
+        <div class="bio">
+           <h3>${firstName}</h3>
+           <ul>
+              <li>Hours in space: 190</li>
+              <li>Active: false</li>
+              <li>Skills: Physician, Chemical Engineer</li>
+           </ul>
+        </div>
+        <img class="avatar" src="images/mae-jemison.jpg">`}
+        })
+        });
+
+
+})
